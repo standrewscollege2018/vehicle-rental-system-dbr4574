1c5c4f33fd5d48f0fa475fbdc14e952ee5c03bd0
MyNameIsKoda <dbr4574@stacmail.net>
2018-06-29 15:21:11 +1200

    Day I
    Commiting to master

--- vms.py
+++ vms.py
@@ -1,1 +1,19 @@
-#YOLO
+class Vechicle:
+
+     def __init__(self, name, seats, ):
+
+        self._name = name
+        self._seats = seats
+        self._avalible = avalible
+
+        students.append(self)
+
+Vehicles[]
+
+def _search(self):
+    while True:
+        try:
+            seats = int(input("Enter the number of seats you would like : "))
+            break
+        except:
+            print("Enter a number between 2-8")
