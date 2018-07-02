1ed5fd55a5859c365aa0a805ac59852b7c21e564
MyNameIsKoda <dbr4574@stacmail.net>
2018-07-03 08:58:02 +1200

    Monday Week 9

--- vms.py
+++ vms.py
@@ -1,19 +1,45 @@
-class Vechicle:
+class Vehicle:
 
-     def __init__(self, name, seats, ):
-
+    def __init__(self, name, seats, plate):
         self._name = name
         self._seats = seats
-        self._avalible = avalible
+        self._plate = plate
+        self._renter = ""
+        self._avalible = True
+        vehicles.append(self)
 
-        students.append(self)
+    def _display_info(self):
+        print("---------------------")
+        print(self._name)
+        print("Seats : {}".format(self._seats))
+        print("Plate : {}".format(self._plate))
 
-Vehicles[]
+    def _display_all(self):
+        print("---------------------")
+        print(self._name)
+        print("Seats : {}".format(self._seats))
+        print("Plate : {}".format(self._plate))
+        if self._avalible == True:
+            print("This vehicle is avalible")
+        else:
+            print("Sorry, this vehicle is unavalible")
 
-def _search(self):
+vehicles = []
+
+Vehicle("2005 Volkswagen Golf", 5, "I 747 I")
+
+def _seats_search(self):
     while True:
         try:
             seats = int(input("Enter the number of seats you would like : "))
             break
         except:
             print("Enter a number between 2-8")
+    search_count=0
+    for v in vehicles:
+        if v._seats == seats:
+            search_count += 1
+            s._display_info()
+    print("{} vehicle(s) found.".format(search_count))
+
+_seats_search()
